54c5ab7785be2fab2abd1410276175a63aa24b64
Usama Muhammad <usamamuhammad45678@gmail.com>
2020-10-07 21:11:06 +0500

    Updates

---
+++ precache-manifest.3a5e725d1627ce10c788dc0d1d9ff94b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8053a6569ecd06ba605da2d02057dd80",
+    "url": "/Carousel-with-AdminPanel/index.html"
+  },
+  {
+    "revision": "9361f1053c259c15baee",
+    "url": "/Carousel-with-AdminPanel/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "bde35170d659496a923d",
+    "url": "/Carousel-with-AdminPanel/static/css/main.0e421966.chunk.css"
+  },
+  {
+    "revision": "9361f1053c259c15baee",
+    "url": "/Carousel-with-AdminPanel/static/js/2.eea96ae1.chunk.js"
+  },
+  {
+    "revision": "999b2829de03f03f5ebf70720cbcd78e",
+    "url": "/Carousel-with-AdminPanel/static/js/2.eea96ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde35170d659496a923d",
+    "url": "/Carousel-with-AdminPanel/static/js/main.f43650ba.chunk.js"
+  },
+  {
+    "revision": "752ea7aec00f5132d8c5",
+    "url": "/Carousel-with-AdminPanel/static/js/runtime-main.0be1e4bb.js"
+  }
+]);
